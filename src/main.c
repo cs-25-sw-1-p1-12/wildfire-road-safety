f9f5d4122cca2423b00ac0f77a51a0e2cf76ca90
atleSkaanes <atle.skaanes@gmail.com>
2025-11-19 13:12:26 +0100

    Removed get_wind_velocity call

--- src/main.c
+++ src/main.c
@@ -27,25 +27,19 @@ int main()
     tempFires.items = malloc(sizeof(FireArea) * 2);
     tempFires.len = 2;
 
-    tempFires.items[0] = (FireArea) {
-        .bbox = (BoundBox) {
-            .c1 = {.lat = 57.008437507228265, .lon = 9.98708721386485},
-            .c2 = { .lat = 57.01467041792688, .lon = 9.99681826817088}
-        },
+    tempFires.items[0] = (FireArea){
+        .bbox = (BoundBox){.c1 = {.lat = 57.008437507228265, .lon = 9.98708721386485},
+                           .c2 = {.lat = 57.01467041792688, .lon = 9.99681826817088}},
         .spread_delta = 0.3,
         .temperature = 326
     };
 
-        tempFires.items[1] = (FireArea) {
-            .bbox = (BoundBox) {
-                .c1 = {.lat = 57.008437507228265, .lon = 9.98708721386485},
-                .c2 = { .lat = 57.01467041792688, .lon = 9.99681826817088}
-            },
-            .spread_delta = 0.7,
-            .temperature = 402
-        };
+    tempFires.items[1] = (FireArea){
+        .bbox = (BoundBox){.c1 = {.lat = 57.008437507228265, .lon = 9.98708721386485},
+                           .c2 = {.lat = 57.01467041792688, .lon = 9.99681826817088}},
+        .spread_delta = 0.7,
+        .temperature = 402
+    };
 
-        assess_roads(&roads, tempFires);
-        Vec2 wind = get_wind_velocity((GCoord){.lat = 0., .lon = 0.});
-        printf("Wind: x = %lf, y = %lf\n", wind.x, wind.y);
-    }
+    assess_roads(&roads, tempFires);
+}
