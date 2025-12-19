6a9d288910544c8b41398a2914505ad7d4fc1b8e
Marlo <marcushvingelby@gmail.com>
2025-12-19 15:13:24 +0100

    Added support for unit tests

---
+++ tests/test.c
@@ -0,0 +1,31 @@
+//
+// Created by Marcus Hvingelby on 19/12/2025.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_assess_road()
+{
+
+}
+
+/*
+void example_test()
+{
+    // Test condition. This should be false
+    if (some_condition) {
+        // Describe the fail cause here, e.g.
+        fprintf(stderr, "Test failed: add(1, 1) != 3. Expected 2\n");
+        exit(1);
+    }
+
+    printf("All tests passed!\n");
+}
+*/
+
+int main()
+{
+    test_assess_road();
+    return 0;
+}
